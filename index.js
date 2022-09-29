8fbd2d5e70f8c7c7b8d059aa5aff91bb1788e556
Alton Fo <altonfo109@gmail.com>
2022-09-29 15:14:40 -0400

    commit tree a3066cb3e81861deefd8de5208eeefb2eeb4f001

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+   const cOne = [...cats]
+   cOne.push(name)
+   return cOne
+}
+function prependCat(name) {
+    const cOne = [...cats]
+    cOne.unshift(name)
+    return cOne
+ }
+ function removeLastCat(name) {
+    const cOne = [...cats]
+    cOne.pop(name)
+    return cOne
+ }
+ function removeFirstCat(name) {
+    const cOne = [...cats]
+    cOne.shift(name)
+    return cOne
+ }
